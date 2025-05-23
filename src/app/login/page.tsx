925762cd571ca9117e82be8c862d77e41ea5b6d1
Dawn <dawnyxl@gmail.com>
2025-05-23 21:49:13 +0800

    Update page.tsx
    Updated Login Page Design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,5 @@
-import { login } from './actions'
+import NewLoginPage from './login'
 
 export default function LoginPage() {
-  return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-    </form>
-  )
+  return <NewLoginPage />
 }
