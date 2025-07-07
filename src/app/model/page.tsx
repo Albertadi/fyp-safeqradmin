819b4628857a93e2b90180039fd3362a2cf3cbf3
Dawn <dawnyxl@gmail.com>
2025-07-07 23:35:55 +0800

    Create page.tsx

---
+++ src/app/model/page.tsx
@@ -0,0 +1,9 @@
+// src/app/ml/page.tsx
+'use client';
+
+import React from 'react';
+import ModelManagement from '@/app/model/ModelManagement';
+
+export default function MLModelPage() {
+  return <ModelManagement />;
+}
