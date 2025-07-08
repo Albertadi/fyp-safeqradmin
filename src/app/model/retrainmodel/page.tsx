7de5033e47a004801cd46f0cf39ae7664af3bb7b
Dawn <dawnyxl@gmail.com>
2025-07-09 00:52:41 +0800

    Create page.tsx

---
+++ src/app/model/retrainmodel/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import RetrainModel from '@/app/model/retrainmodel/retrainModel';
+
+export default function RetrainModelPage() {
+  return <RetrainModel />;
+}
