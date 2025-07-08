30fb7aadf81f981d08f1031cac4175ced5760540
Dawn <dawnyxl@gmail.com>
2025-07-09 00:52:34 +0800

    Create page.tsx

---
+++ src/app/model/deletemodel/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import DeleteModel from '@/app/model/deletemodel/deleteModel';
+
+export default function DeleteModelPage() {
+  return <DeleteModel />;
+}
