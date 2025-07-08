3f68fa8d16d25233353135359a20ab2039545906
Dawn <dawnyxl@gmail.com>
2025-07-09 05:08:58 +0800

    Delete page.tsx

--- src/app/model/deletemodel/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import DeleteModel from '@/app/model/deletemodel/deleteModel';
-
-export default function DeleteModelPage() {
-  return <DeleteModel />;
-}
